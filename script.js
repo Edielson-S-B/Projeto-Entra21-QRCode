53255ac8e4f6a99d766478f59d2a01ac86c01224
Edielson-S-B <108365306+Edielson-S-B@users.noreply.github.com>
2022-09-02 16:36:06 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function GerarQRCode(){
-var GoogleChartAPI='https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=https://www.youtube.com/'
-document.querySelector('#QRCodeImage').src = GoogleChartAPI;
-}
